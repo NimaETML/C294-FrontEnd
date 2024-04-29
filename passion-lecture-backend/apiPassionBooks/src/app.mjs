ae8b9fcd517defed0eea879904de6b993c6016cd
darchasi <pv20qck@eduvaud.ch>
2024-04-29 11:32:25 +0200

    Commit apiPassionBooks

---
+++ passion-lecture-backend/apiPassionBooks/src/app.mjs
@@ -0,0 +1,64 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const port = 3901;
+import { initDb, sequelize } from "./db/sequelize.mjs";
+
+sequelize
+  .authenticate()
+  .then((_) =>
+    console.log("La connexion à la base de données a bien été établie")
+  )
+  .catch((error) => {
+    console.log(error);
+    console.error("Impossible de se connecter à la DB");
+  });
+
+initDb();
+
+// message affiché par défaut
+app.get("/", (req, res) => {
+  res.send("API REST pour visualier et noter des livres !");
+});
+
+// si /api/ alors redirect à l'adresse de base
+app.get("/", (req, res) => {
+  res.redirect(`http://localhost:${port}/`);
+});
+
+// si /api/books/ alors envoi dans la route booksRouter
+import { booksRouter } from "./routes/books.mjs";
+app.use("/api/books", booksRouter);
+
+// si /api/categories/ alors envoi dans la route categoriesRouter
+import { categoriesRouter } from "./routes/categories.mjs";
+app.use("/api/categories", categoriesRouter);
+
+// si /api/authors/ alors envoi dans la route writersRouter
+import { writersRouter } from "./routes/writers.mjs";
+app.use("/api/authors", writersRouter);
+
+// si /api/login/ alors envoi dans la route loginRouter
+import { loginRouter } from "./routes/login.mjs";
+app.use("/api/login", loginRouter);
+
+// si /api/users/ alors envoi dans la route usersRouter
+import { usersRouter } from "./routes/users.mjs";
+app.use("/api/users", usersRouter);
+
+// Si aucune route ne correspondant à l'URL demandée par le consommateur
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée, veuillez réesayer";
+  res.status(404).json({ message });
+});
+
+app.listen(port);
+
+/* SI TEMPS
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}/`);
+});
+ */
---
+++ passion-lecture-backend/apiPassionBooks/src/db/mock-writers.mjs
@@ -0,0 +1,28 @@
+let writers = [
+        {
+          id: 1,
+          firstName: "John",
+          lastName: "Doe"
+        },
+        {
+          id: 2,
+          firstName: "John2",
+          lastName: "Doe2"         
+        },
+        {
+          id: 3,
+          firstName: "John3",
+          lastName: "Doe3"
+        },
+        {
+          id: 4,
+          firstName: "John4",
+          lastName: "Do4"
+        },
+        {
+          id: 5,
+          firstName: "John5",
+          lastName: "Doe5"
+        }
+]
+export {writers}
